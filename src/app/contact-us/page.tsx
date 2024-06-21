0bd0c6c1f914680a9bf5054ca5cf875d765971b3
Olawande Akinmosin <samueldacoal@gmail.com>
2024-06-21 13:24:51 +0100

    ui updates

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,9 +1,13 @@
 import ContactCard from "@/components/Contact/Card";
 import ContactForm from "@/components/Contact/ContactForm";
+import Navbar from "@/components/landing/Navbar";
 
 const ContactUs = () => {
 	return (
 		<section>
+			<div className="mb-20">
+				<Navbar isLight />
+			</div>
 			<section className="flex min-h-[120vh] items-center pb-32">
 				<div className="basis-3/5">
 					<ContactForm />
