a1c7bc1e9eec0d811ba4e448d9ce48673cb236c2
Olawande Akinmosin <samueldacoal@gmail.com>
2024-06-21 13:22:37 +0100

    ui updates

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -1,10 +1,14 @@
 import Portal from "@/components/Portal";
 import Jobs from "@/components/careers/Jobs";
 import WhyJoin from "@/components/careers/WhyJoin";
+import Navbar from "@/components/landing/Navbar";
 
 const Careers = () => {
 	return (
 		<div>
+			<div className="mb-20">
+				<Navbar isLight />
+			</div>
 			<Portal
 				title="We are looking for talented people"
 				text="At Chris Ejik Group, we believe in empowering lives and energizing futures through access to essential healthcare and electrical power services and goods. Emphasize the brand's focus on innovation and technology to provide sustainable solutions for a better future."
