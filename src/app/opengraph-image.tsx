bba949c0f0e6d19ba1532636001ff46476126c95
Olawande Akinmosin <samueldacoal@gmail.com>
2024-07-23 20:46:23 +0100

    opengraph image

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,13 +1,19 @@
 import { ImageResponse } from "next/og";
+
 export const runtime = "edge";
+
 export const contentType = "image/png";
+
 export default async function OpenGraphImage() {
+	const outfitSemiBold = fetch(
+		new URL("./Outfit-SemiBold.ttf", import.meta.url),
+	).then((res) => res.arrayBuffer());
 	const Image = new ImageResponse(
 		(
 			<div
 				style={{
-					fontSize: 128,
-					background: "red",
+					fontSize: 108,
+					background: "#eee",
 					width: "100%",
 					height: "100%",
 					display: "flex",
@@ -16,12 +22,20 @@ export default async function OpenGraphImage() {
 					justifyContent: "center",
 				}}
 			>
-				Ejik Group
+				<p className="text-accent">Ejik Group of Companies</p>
 			</div>
 		),
 		{
 			width: 1200,
 			height: 600,
+			fonts: [
+				{
+					name: "Outfit",
+					data: await outfitSemiBold,
+					style: "normal",
+					weight: 400,
+				},
+			],
 		},
 	);
 	return Image;
