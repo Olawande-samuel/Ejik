7ae895b17f5055520d9e0df26f17b6649d7b7059
Olawande Akinmosin <samueldacoal@gmail.com>
2024-07-29 20:54:28 +0100

    OG updates

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,19 +4,19 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 	return [
 		{
 			url: process.env.NEXT_PUBLIC_WEBSITE_URL as string,
-			lastModified: new Date(),
+			lastModified: new Date("2024-07-29"),
 		},
 		{
 			url: `${process.env.NEXT_PUBLIC_WEBSITE_URL}/about-us` as string,
-			lastModified: new Date(),
+			lastModified: new Date("2024-07-29"),
 		},
 		{
 			url: `${process.env.NEXT_PUBLIC_WEBSITE_URL}/careers` as string,
-			lastModified: new Date(),
+			lastModified: new Date("2024-07-29"),
 		},
 		{
 			url: `${process.env.NEXT_PUBLIC_WEBSITE_URL}/contact-us` as string,
-			lastModified: new Date(),
+			lastModified: new Date("2024-07-29"),
 		},
 	];
 }
